a188be3b231f5152259d45ef6f1a52f67d347d86
jesuszdp <jesusz.unam@gmail.com>
2026-04-30 11:40:19 -0600

    Se agregan access points del tipo de curso Programa anual, aplicativo SIPEC

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -23,3 +23,15 @@ $routes->post('abec/estadisticas/sexo', 'Abec::porSexo');
 $routes->post('abec/listadoCursos', 'Abec::listadoCursos');
 $routes->get('abec/detalleAlumnos/(:num)', 'Abec::detalleAlumnos/$1');
 
+// ========== ENDPOINT: ProgramaAnual (Mejorado con seguridad) ==========
+$routes->post('programa/obtenerCatalogos', 'ProgramaAnual::obtenerCatalogos');
+// Estadísticas agregadas con filtros opcionales (anios, id_ooad, id_umae, id_tipo_sesion)
+$routes->post('programa/obtenerEstadisticas', 'ProgramaAnual::obtenerEstadisticas');
+// Agregados: informes y listados derivados de vw_abec_curso_alumno
+$routes->post('programa/estadisticas/ooad', 'ProgramaAnual::estadisticasPorOOAD');
+$routes->post('programa/estadisticas/umae', 'ProgramaAnual::estadisticasPorUMAE');
+$routes->post('programa/estadisticas/categoria', 'ProgramaAnual::categoriaPorAlumno');
+$routes->post('programa/estadisticas/mes', 'ProgramaAnual::porMes');
+$routes->post('programa/estadisticas/sexo', 'ProgramaAnual::porSexo');
+$routes->post('programa/listadoCursos', 'ProgramaAnual::listadoCursos');
+$routes->get('programa/detalleAlumnos/(:num)', 'ProgramaAnual::detalleAlumnos/$1');
